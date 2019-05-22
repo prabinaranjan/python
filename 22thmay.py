e38786252ad7fedf5563f25c716bc976610d2414
prabina ranjan <prabinaranjanpatro@gmail.com>
2019-05-22 20:58:20 +0530

    list

---
+++ 22thmay.py
@@ -0,0 +1,15 @@
+
+'''list1=[]
+
+for i in myList:
+    if(i%2 !=0):
+        myList.remove(i)
+
+print(myList)
+for i in myList:
+    if(i%2==0):
+        list1.append(i)
+print(list1)'''
+myList=[1,2,3,]
+myList[::-1]
+print(myList)
